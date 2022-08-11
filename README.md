ae234d59aa7559bed57f98c128b4f2bbd6f4711b
M Nascimento Rocha <m.nascimento.rocha@avanade.com>
2022-08-11 17:25:13 -0300

    feat: I added the start code for project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SignalR_Poc
+
 This is just a  Proof of concept for testing signalR in dotnet core.
+
+
+<img src="/img/working.png" alt="signalR running" style="height: 100px; width:100px;"/>
---
+++ src/ConsoleClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a key to start listening..");
+            Console.ReadKey();
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:60907/coffeehub")
+                .AddMessagePackProtocol()
+                .Build();
+
+            connection.On<Order>("NewOrder", (order) =>
+            {
+                var consoleColors = Enum.GetValues(typeof(ConsoleColor));
+                var color = (ConsoleColor)new Random().Next(1, consoleColors.Length);
+                Console.ForegroundColor = color;
+                Console.WriteLine($"Somebody ordered an {order.Product}");
+            });
+
+            connection.StartAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Listening. Press a key to quit");
+            Console.ReadKey();
+        }
+    }
+}
